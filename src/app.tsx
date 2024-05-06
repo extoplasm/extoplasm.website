65d5b16bedc43efea9101f86f307b902b20fa2c6
extoplasm <pleassehelsp@gmail.com>
2024-05-07 08:08:58 +1000

    i dont like webdev anymore

--- src/app.tsx
+++ src/app.tsx
@@ -15,7 +15,7 @@ function App() {
         <p>extoplasm</p>
         <button onClick={(() => {
           document.getElementById('scene1')!.style.visibility = 'hidden';
-          setTimeout(()=> {
+          setTimeout(() => {
             document.getElementById('scene2')!.style.visibility = 'visible';
           }, 1000)
         })}>log in</button>
@@ -24,13 +24,34 @@ function App() {
     <div id="scene2">
       <div id="sidebar">
         <button onClick={(()=> {
-          setClicked(clicked ? 0 : 1)
+          setClicked(clicked ? 0 : 1);
+          (document.querySelector('#scene2 > #sidebar > button') as any)!.disabled = true;
+          document.getElementById('terminal-window')!.style.visibility = 'visible';
+          let i = 0
+          let text = './index.html'
+          const typingEffect  = () => {
+            if (i < text.length) {
+              document.querySelector('#terminal-window > #window-content > #terminal-text')!.innerHTML += text.charAt(i);
+              i++;
+              setTimeout(typingEffect, 50)
+            }
+          }
+          setTimeout(typingEffect, 1000);
         })}>
           <div id="icon">
             <img src={clicked ? terminalIconActive : emptyIcon} alt="active" id='active-dot'/>
             <img src={terminalIcon} alt="terminal icon" />
           </div>
         </button>
+      </div>
+    </div>
+    <div id="terminal-window">
+      <div id="window-header">
+        <p>terminal</p>
+        <p>x</p>
+      </div>
+      <div id="window-content">
+        <p id="terminal-text">extoplasm:-$ </p>
       </div>
     </div>
   </>
